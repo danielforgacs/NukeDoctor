dd8590c28dec4bfa26c6af77a14919a93e0d98c4
df@ <@>
2022-10-18 14:38:13 +0100

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+mod modules {
+    pub use std::path::Path;
+    pub use std::fs::File;
+    pub use std::io::prelude::*;
+}
+
+use modules::*;
+
+fn main() {}
+
+fn read_file_to_string(path: &str) -> String {
+    let mut buf = String::new();
+    let mut file_handle = File::open(path)
+        .expect(format!("Error opening file for reading: {}", path).as_str());
+    let _read_bytes = file_handle.read_to_string(&mut buf)
+        .expect(format!("Error reading file: {}", path).as_str());
+    buf
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_read_file_to_string() {
+        let expected = r#"Dot {
+  name alskdjfhklasdf
+}"#;
+        let data = read_file_to_string("test_data/scene01.nk");
+        assert_eq!(data, expected);
+    }
+
 }
