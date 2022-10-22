f4ddc1017f76deefec6e0a7d5529c1dd1259c8b5
df@ <@>
2022-10-22 21:57:10 +0100

    getting and writing name ok

--- src/parser.rs
+++ src/parser.rs
@@ -3,21 +3,28 @@ use crate::modules::*;
 pub fn parse(source: Vec<char>) -> Vec<Node> {
     let mut nodes = Vec::with_capacity(5000);
     let mut index = 0;
+    let mut group: Option<String> = Option::None;
     loop {
         let char = source[index];
         if char.is_alphabetic() {
             let word = extract_word(&source, &mut index);
             log::debug!("found word: {}, index: {}", &word, &index);
             if source[index..=index+1] == [' ', '{'] {
+                if word == "Group" {
+                    group = Option::Some(word.clone());
+                } else if word == "end_group" {
+                    group = Option::None;
+                }
                 skip_whitespace(&source, &mut index);
                 let body_index = index.clone() + 1;
-                let body = parse_brackets(&source, &mut index);
+                let (name, body) = parse_brackets(&source, &mut index);
                 log::debug!("extracted body. index: {}", &index);
                 nodes.push(Node::new(
                     word,
+                    name,
                     body,
                     body_index,
-                    Option::None,
+                    group.clone(),
                 ));
             }
         }
@@ -58,26 +65,40 @@ fn skip_whitespace(source: &Vec<char>, index: &mut usize) {
     }
 }
 
-fn parse_brackets(source: &Vec<char>, index: &mut usize) -> String {
+fn parse_brackets(source: &Vec<char>, mut index: &mut usize) -> (String, String) {
     let mut body = String::with_capacity(10000);
+    let mut name = String::with_capacity(100);
     let mut nesting = 1;
     *index += 1;
     loop {
         let char = source[*index];
-        if char == '{' {
+        if char.is_alphanumeric() {
+            let word = extract_word(&source, &mut index);
+            body += &word.as_str();
+            *index -= 1;
+            if word == "name" {
+                log::info!("getting name. index: {}", index);
+                name = extract_word(&source, &mut index);
+                *index -= 1;
+            }
+        } else if char == '{' {
             nesting += 1;
-        }
-        if char == '}' {
+            if nesting != 0 {
+                body.push(char);
+            }
+        } else if char == '}' {
             nesting -= 1;
             if nesting == 0 {
                 break;
             }
+            body.push(char);
+        } else {
+            body.push(char);
         }
-        body.push(char);
         *index += 1;
         if *index == source.len() - 1 {
             break;
         }
     }
-    body
+    (name, body)
 }
