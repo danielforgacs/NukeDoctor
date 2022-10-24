6da89355cfc28fa1e26e7189215b862063618f61
df@ <@>
2022-10-24 19:14:00 +0100

    wip config

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,8 @@
-struct Config {
+use crate::modules::*;
+
+#[derive(Debug)]
+pub struct Config {
+    script: String,
     ignore_commands: bool,
     write_empty_ignored_nodes: bool,
     ignore_node_types: Vec<String>,
@@ -6,12 +10,28 @@ struct Config {
 }
 
 impl Config {
-    fn new() -> Self {
+    fn new(path: String) -> Self {
         Self {
+            script: path,
             ignore_commands: false,
             write_empty_ignored_nodes: false,
             ignore_node_types: Vec::new(),
             max_body_lines: 1000,
         }
     }
+}
+
+pub fn get_config() -> Config {
+    let matches = Command::new("nukedoctor")
+        .name("nukedoctor")
+        .about("about stuff alksdfhj laskdfh alkjdfh aksdhf")
+        .version(env!("CARGO_PKG_VERSION"))
+        .args([
+            Arg::new("scene")
+            .required(true)
+        ])
+        .get_matches();
+
+    let scene = matches.get_one::<String>("scene").unwrap().to_owned();
+    Config::new(scene)
 }
