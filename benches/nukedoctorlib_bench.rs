76c13de77e320a70c8d77a836cc06d095db47bff
df@ <@>
2022-10-27 22:34:10 +0100

    fixed bench test

--- benches/nukedoctorlib_bench.rs
+++ benches/nukedoctorlib_bench.rs
@@ -1,10 +1,10 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use nukedoctor::utils::clean_up_scene;
+use nukedoctor::utils::{clean_up_scene, read_file_to_string};
 use nukedoctor::config::Config;
 
 pub fn bench_clean_up_scene(c: &mut Criterion) {
     c.bench_function("clean_up_scene() bench", |b| b.iter(|| clean_up_scene(
-        "test_data/bench_data.nk".to_string(),
+        read_file_to_string("test_data/bench_data.nk").unwrap(),
         Config::new("test_data/bench_data.nk".to_string())
     )));
 }
