81feda7221f4118dc97aadc7dc4453762d5a436c
df@ <@>
2022-10-27 10:25:03 +0100

    ..

--- benches/nukedoctorlib_bench.rs
+++ benches/nukedoctorlib_bench.rs
@@ -4,8 +4,8 @@ use nukedoctor::config::Config;
 
 pub fn bench_clean_up_scene(c: &mut Criterion) {
     c.bench_function("clean_up_scene() bench", |b| b.iter(|| clean_up_scene(
-        "/home/ford/storage/dev/NukeDoctor/test_data/Cloudtastic.nk".to_string(),
-        Config::new("/home/ford/storage/dev/NukeDoctor/test_data/Cloudtastic.nk".to_string())
+        "test_data/Cloudtastic.nk".to_string(),
+        Config::new("test_data/Cloudtastic.nk".to_string())
     )));
 }
 
